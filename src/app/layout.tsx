816e5362554f23797af3a86814f632384d5798a7
Sanchit Kathpalia <sanchitkhthpalia@gmail.com>
2025-12-04 16:06:32 +0530

    Fix Next.js layout typing for Vercel build

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
       <body className={inter.className}>
       <NextTopLoader />
       <AuthDialogProvider>
-      <SessionProviderComp>
+      <SessionProviderComp session={undefined}>
         <ThemeProvider
           attribute="class"
           enableSystem={true}
